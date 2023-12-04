8d3305ca8fd2131f9d97d817d5b6bff6a5933e29
Rakib Shanto <rakibshanto96@gmail.com>
2023-12-04 19:09:49 +0600

    completed the css portion of testimonial section

--- src/components/GetOurApp.jsx
+++ src/components/GetOurApp.jsx
@@ -2,22 +2,24 @@ import React from "react";
 
 const GetOurApp = () => {
   return (
-    <div className="wrapper">
-      <div className="app-section">
-        <h2>Get Our App</h2>
-        <p>You can use our App for better experience on smartphones</p>
-        <div className="app-buttons">
-          <div className="app-btn">
-            <img src="/assets/images/apple-store.svg" alt="" />
-            <span>App Store</span>
-          </div>
-          <div className="app-btn">
-            <img src="/assets/images/play-store.svg" alt="" />
-            <span>Google Play</span>
+    <section>
+      <div className="wrapper">
+        <div className="app-section">
+          <h2>Get Our App</h2>
+          <p>You can use our App for better experience on smartphones</p>
+          <div className="app-buttons">
+            <div className="app-btn">
+              <img src="/assets/images/apple-store.svg" alt="" />
+              <span>App Store</span>
+            </div>
+            <div className="app-btn">
+              <img src="/assets/images/play-store.svg" alt="" />
+              <span>Google Play</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,44 +2,52 @@ import React from "react";
 
 const Testimonials = () => {
   return (
-    <div className="wrapper">
-      <div className="testimonials-section">
-        <div className="left">
-          <div className="testimonial-card">
-            <div className="content">
-              This is a great course. It helped me a lot. Thank you :)
+    <section>
+      <div className="wrapper">
+        <div className="testimonials-section">
+          <div className="left">
+            <div className="testimonial-card">
+              <div className="content">
+                This is a great course. It helped me a lot. Thank you :)
+              </div>
+              <div className="info">
+                <h4>Jane Cooper</h4>
+                <p className="company">Developer, Sony</p>
+              </div>
+              <img src="/assets/images/testimonial-image-1.png" alt="" />
             </div>
-            <div className="info">
-              <h4>Jane Cooper</h4>
-              <p className="company">Developer, Sony</p>
+            <div className="testimonial-card">
+              <div className="content">Amazing Work! Well done!</div>
+              <div className="info">
+                <h4>Jacob Jones</h4>
+                <p className="company">Designer, Facebook</p>
+              </div>
+              <img src="/assets/images/testimonial-image-2.png" alt="" />
             </div>
-            <img src="/assets/images/testimonial-image-1.png" alt="" />
           </div>
-          <div className="testimonial-card">
-            <div className="content">Amazing Work! Well done!</div>
-            <div className="info">
-              <h4>Jacob Jones</h4>
-              <p className="company">Designer, Facebook</p>
+          <div className="right">
+            <h2>What our students say?</h2>
+            <div className="features">
+              <p>
+                All students get access to all the videos and also the source
+                code of the projects.
+              </p>
+              <p>
+                You will also have access to a private Discord channel where you
+                can discuss your doubts.
+              </p>
             </div>
-            <img src="/assets/images/testimonial-image-2.png" alt="" />
+
+            <a href="#" className="btn light desktop-btn">
+              Learn More
+            </a>
           </div>
+          <a href="#" className="btn light mobile-btn">
+            Learn More
+          </a>
         </div>
-        <div className="right">
-          <h2>What our students say</h2>
-          <p>
-            All students get access to all the videos and also the source code
-            of the projects.
-          </p>
-          <p>
-            You will also have access to a private Discord channel where you can
-            discuss your doubts.
-          </p>
-        </div>
-        <a href="#" className="btn light">
-          Learn More
-        </a>
       </div>
-    </div>
+    </section>
   );
 };
 
